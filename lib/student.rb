a60c4ca1d21a35d82826af016b17593a9238e6d3
chic-state-0895 <brookeabryski@gmail.com>
2020-09-27 19:45:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |k, v|
-      self.send ("#{key}=", value)
+      self.send("#{key}=", value)
   end
 
   def self.create_from_collection(students_array)
